fd5f2b9c7aa30433d9cc40df29bf0ea84d68ba63
tfaaguilar <e_aguilarvelez@fanshaweonline.ca>
2023-03-31 21:35:03 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -1,29 +1,9 @@
-let member = {
-    president : {
-        name: "Matt",
-        headline: "President",
-        bio: "Matt has overseen the growth and success of the agency and he has developed a wide range of skills working across diverse markets and brands on both a local and national level for consumer and clients. Today Matt facilitates creative thinking with clients and her team to explore and discover better ways to grow businesses and brands in the digital space.",
-        
-    },
+let heroButtons = document.querySelectorAll('.characters a'),
+    lightBox = document.querySelector('#lightbox');
 
-    director : {
-        name: "David",
-        headline: "Director",
-        bio: "He's an expert project manager with a knack for turning complex problems into manageable tasks, his breadth of knowledge in the development space puts clients at ease and keeps projects on track. David holds a Bachelor of Business Management and Organizational Studies from Western University.",
-    },
-
-    deveolper : {
-        name: "Wendy",
-        headline: "Developer",
-        bio: "Wendy holds a bachelor's degree in Media Studies with a focus on Public Relations from the University of Guelph as well as a Media Communications diploma from Humber Institute of Technology and Advanced Learning. Her areas of expertise include website strategy and architecture, attention to detail, organization and creative writing. Stephanie is responsible for overseeing the website accounts team and release of all digital content",
-        
-    },
-
-    designer : {
-        name: "Michelle",
-        headline: "Designer",
-        bio: "Michelle brings over a decade of digital marketing experience to Red Rhino. Previous to Red Rhino he has worked freelance, client-side, and with multiple agencies. Mike has delivered award-winning work for groups of all sizes, spanning local businesses to global Fortune 500 enterprises. His strong technical background helps him bring industry-leading solutions to the most complex challenges in the areas of SEO, analytics, and automation.",
-        
-    }
-    
+function loadHeroData() {
+    lightBox.querySelector('h3').textContent = members[this.dataset.member].headline;
+    lightBox.querySelector('p').textContent = members[this.dataset.member].bio;
 }
+
+heroButtons.forEach(hero => hero.addEventListener('click', loadHeroData));
