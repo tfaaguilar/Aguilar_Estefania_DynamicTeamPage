600c3a6b2269ea301229a824374f7651c32d6d28
tfaaguilar <e_aguilarvelez@fanshaweonline.ca>
2023-04-01 12:33:17 -0400

    Css
    Update

--- js/members.js
+++ js/members.js
@@ -1,6 +1,6 @@
 let character = {
     president : {
-        name: "Matt",
+        name: "Matt Walker",
         headline: "President",
         bio: "He has overseen the growth and success of the agency and he has developed a wide range of skills working across diverse markets and brands on both a local and national level for consumer and clients. Today Matt facilitates creative thinking with clients and her team to explore and discover better ways to grow businesses and brands in the digital space. He is:",
         
@@ -8,20 +8,20 @@ let character = {
     },
 
     director : {
-        name: "David",
+        name: "David Harrison",
         headline: "Director",
-        bio: "He's an expert project manager with a knack for turning complex problems into manageable tasks, his breadth of knowledge in the development space puts clients at ease and keeps projects on track. David holds a Bachelor of Business Management and Organizational Studies from Western University. He is",
+        bio: "He's an expert project manager with a knack for turning complex problems into manageable tasks, his breadth of knowledge in the development space puts clients at ease and keeps projects on track. David holds a Bachelor of Business Management and Organizational Studies from Western University. He is:",
     },
 
     developer : {
-        name: "Wendy",
+        name: "Wendy Harper",
         headline: "Developer",
         bio: "She holds a bachelor's degree in Media Studies with a focus on Public Relations from the University of Guelph as well as a Media Communications diploma from Humber Institute of Technology and Advanced Learning. Her areas of expertise include website strategy and architecture, attention to detail, organization and creative writing. She is:",
         
     },
 
     designer : {
-        name: "Michelle",
+        name: "Michelle Wright",
         headline: "Designer",
         bio: "She has worked freelance, client-side, and with multiple agencies and has delivered award-winning work for groups of all sizes, spanning local businesses to global Fortune 500 enterprises. Her strong technical background helps her bring industry-leading solutions to the most complex challenges in the areas of SEO, analytics, and automation. She is:",
         
